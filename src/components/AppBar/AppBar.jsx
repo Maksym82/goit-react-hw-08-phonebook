1beeb2d5c8f27c6dd3bde08d58d0ae6f22eb04ee
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-23 23:09:51 +0300

    Create AppBar

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { BiSolidUserCircle, BiSolidDoorOpen } from 'react-icons/bi';
+
+import { getIsLoggedIn, getUser } from 'redux/auth/auth-selectors';
+import { logOut } from 'redux/auth/auth-operations';
+import { Navigation } from 'components/Navigation/Navigation';
+import { AuthNavigation } from 'components/AuthNavigation/AuthNavigation';
+
+export const AppBar = () => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const dispatch = useDispatch();
+  const user = useSelector(getUser);
+
+  return (
+    <Header>
+      <Navigation />
+      {isLoggedIn ? (
+        <UserMenu>
+          <User>
+            <BiSolidUserCircle />
+            {user.name}
+          </User>
+          <Button type="button" onClick={() => dispatch(logOut())}>
+            <BiSolidDoorOpen />
+            <Span>Logout</Span>
+          </Button>
+        </UserMenu>
+      ) : (
+        <AuthNavigation />
+      )}
+    </Header>
+  );
+};
