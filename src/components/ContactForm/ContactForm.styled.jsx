34200732f5bc5db45f2f9cb8a09d443a6a59aaa5
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 21:36:17 +0300

    fix

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,52 +1,51 @@
 import styled from '@emotion/styled';
 
-
 export const Form = styled.form`
-    max-width: 350px;
-    margin: 0 auto;
-    border: 1px solid gray;
-    border-radius: 10px;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-shadow: 1px 1px 5px gray;
-    background: #b2c4d4;
-`;
-
-export const FormInput = styled.input`
-    max-width: 300px;
-    width: 100%;
-    height: 30px;
-    border-radius: 8px;
-    font-size: 20px;
-    padding: 10px;
+  max-width: 350px;
+  margin: 0 auto;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 1px 1px 5px gray;
+  background: linear-gradient(180deg, #b5b5b5, #599caa, #0f596b);
 `;
 
 export const FormLabel = styled.label`
-    font-size: 28px;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 15px;
-    gap: 5px;
+  font-size: 28px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  gap: 5px;
+`;
+
+export const FormInput = styled.input`
+  max-width: 300px;
+  width: 100%;
+  height: 30px;
+  border-radius: 8px;
+  font-size: 20px;
+  padding: 10px;
 `;
 
 export const FormButton = styled.button`
-    margin: 0 auto;
-    border-radius: 8px;
-    padding: 5px 10px;
-    background: transparent;
-    color: white;
-    box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+  margin: 0 auto;
+  border-radius: 8px;
+  padding: 5px 10px;
+  background: transparent;
+  color: white;
+  box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
     1px 4px 6px rgba(0, 0, 0, 0.16);
-    text-shadow: 5px 5px 10px black;
-    font-size: 18px;
+  text-shadow: 5px 5px 10px black;
+  font-size: 18px;
 
-    :hover,
-    :focus {
+  :hover,
+  :focus {
     cursor: pointer;
     background: #145d6f;
-    }
+  }
 `;
 
 
