76d1f0f93098de8269b014d941d999388a17617a
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 15:54:03 +0300

    fix

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Title = styled.h2`
-    text-align: center;
     color: #181717;
+    text-align: center;
+    text-shadow: 2px 2px 5px white;
     font-size: 30px;
 `;
