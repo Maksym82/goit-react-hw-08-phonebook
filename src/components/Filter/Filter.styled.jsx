558d90a27636ef8bd69cc29b0812d71f4fc0e033
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 21:40:18 +0300

    fix

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -1,22 +1,21 @@
 import styled from '@emotion/styled';
 
-
 export const Container = styled.div`
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 `;
 
 export const FilterLabel = styled.label`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    font-size: 26px;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-size: 26px;
+  align-items: center;
 `;
 
 export const FilterInput = styled.input`
-    max-width: 300px;
-    width: 100%;
-    height: 30px;
-    font-size: 24px;
-    border-radius: 8px;
+  max-width: 300px;
+  width: 100%;
+  height: 30px;
+  font-size: 24px;
+  border-radius: 8px;
 `;
