a2e65d4f8dd1c7bbb551e6e84bafe3ae80f10df2
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 11:20:09 +0300

    update ContactForm style

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -2,40 +2,51 @@ import styled from '@emotion/styled';
 
 
 export const Form = styled.form`
+    max-width: 350px;
+    margin: 0 auto;
+    border: 1px solid gray;
+    border-radius: 10px;
+    padding: 20px;
     display: flex;
     flex-direction: column;
-    padding: 14px;
     align-items: center;
-    background-color: #73bf20;
-    border: 1px solid black;
-    border-radius: 5px;
+    box-shadow: 1px 1px 5px gray;
+    background: #b2c4d4;
 `;
 
 export const FormInput = styled.input`
-    width: 350px;
-    height: 40px;
-    border-radius: 5px;
+    max-width: 300px;
+    width: 100%;
+    height: 30px;
+    border-radius: 8px;
     font-size: 20px;
+    padding: 10px;
 `;
 
 export const FormLabel = styled.label`
+    font-size: 28px;
     display: flex;
     flex-direction: column;
-    margin-bottom: 20px;
-    font-size: 26px;
     margin-bottom: 15px;
+    gap: 5px;
 `;
 
 export const FormButton = styled.button`
     margin: 0 auto;
-    padding: 7px 10px;
-    border-radius: 6px;
-    border: 1px solid tomato;
-    color:blue;
-    background-color: green;
-    cursor: pointer;
-    font-size: 20px;
+    border-radius: 8px;
+    padding: 5px 10px;
+    background: transparent;
+    color: white;
+    box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 4px 6px rgba(0, 0, 0, 0.16);
+    text-shadow: 5px 5px 10px black;
+    font-size: 18px;
 
+    :hover,
+    :focus {
+    cursor: pointer;
+    background: #145d6f;
+    }
 `;
 
 
