b974b9deef98a2504473a4d391debdbc6804fd6e
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 09:37:04 +0300

    fix

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,6 +5,7 @@ import { getIsLoggedIn, getUser } from 'redux/auth/auth-selectors';
 import { logOut } from 'redux/auth/auth-operations';
 import { Navigation } from 'components/Navigation/Navigation';
 import { AuthNavigation } from 'components/AuthNavigation/AuthNavigation';
+import { Button, Header, Span, User, UserMenu } from './AppBar.styled';
 
 export const AppBar = () => {
   const isLoggedIn = useSelector(getIsLoggedIn);
