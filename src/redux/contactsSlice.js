83ded94db136ccf186911f963749913a36ddfd95
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-25 01:09:06 +0300

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -27,12 +27,12 @@ const handleAddContactsFulfilled =(state, action) => {
 };
 
 const handleDeleteContactsFulfilled = (state, action) => {
-  console.log(action.payload.id);
+  // console.log(action.payload.id);
   state.isLoading = false;
   const deleteIdx = state.contactsItem.findIndex(
     contact => contact.id === action.payload.id
   );
-  console.log(deleteIdx);
+  // console.log(deleteIdx);
   state.contactsItem.splice(deleteIdx, 1);
 };
 
