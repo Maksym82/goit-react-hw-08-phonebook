88755efcb836d06fe8fc522d11d019d66242032f
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-24 21:20:33 +0300

    fix

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,11 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-    width: 500px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #7373a1;
-    border-radius: 6px;
-    
+  background-color: #8eb8d3;
+  margin: 0 auto;
+  padding: 20px;
+  width: 450px;
+  border-radius: 8px;
+  box-shadow: 4px 1px 4px rgba(0, 0, 0, 0.12), 0px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 4px 6px rgba(0, 0, 0, 0.16);
 `;
 
